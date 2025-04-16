d5c72146e0b2d0c5d395993cb6dfa50593b6dc85
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2025-04-16 20:38:29 +0300

    update

---
+++ cas/cas_schema.js
@@ -0,0 +1,74 @@
+const schema = {
+
+  meta_context: {
+
+    methodological_crisis_context: {
+      problem_with_reductionism,
+      fragmentation_and_isolation,
+      institutional_conservatism,
+      exhaustion_with_abstraction,
+      toward_holism,
+      need_for_new_scientist
+    },
+
+    purpose_and_significance,
+    philosophical_inspiration,
+    worldview_shift,
+    evolutionary_context
+  },
+
+  features: {
+
+    core_features: {
+
+      foundational_elements: {
+        component,
+        agent
+      },
+
+      organizational_context: {
+        hierarchical_structure,
+        niche_diversity,
+        domain_diversity
+      },
+
+      system_properties: {
+
+        conditions_of_emergence: {
+          autocatalysis,
+          nonlinearity_and_chaos,
+          phase_transition
+        },
+
+        structural_properties: {
+          adaptability,
+          coevolving_environment,
+          distributed_control,
+          self_organization,
+          spontaneous_organization
+        },
+
+        dynamic_behavior: {
+          non_equilibrium_dynamics,
+          emergence,
+          collective_behavior
+        },
+
+        outcomes_and_effects
+      }
+    }
+  },
+
+  components: {
+
+    agent: {
+
+      behavior_archetypes: {
+        none,
+        reactive,
+        modeling
+      },
+
+    }
+  }
+}
